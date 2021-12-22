e5f47df54d662de256619658d9e685d23cf57c5e
Anbuvenkatesh A <anbuvenkatesh.a@gmail.com>
2021-12-22 20:14:55 +0530

    Clean up of docker image build

--- weather-prediction-infra/src/main/resources/docker/weather-app-publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-if [ -z "$DOCKER_ENV_LOCATION" ]; then export DOCKER_ENV_LOCATION=config ;fi
-
-# Copy the environment file for docker to resolve
-cp -f ${DOCKER_ENV_LOCATION}/config.env .env
-
-# Now run Docker Compose
-docker compose -f weather-service.yaml push
